56f919fcfacf756633d7b5fe31c678db9f125111
qhauuy <qhauuy@student.42mulhouse.fr>
2026-01-25 17:15:05 +0100

    blabla

--- a.sh
+++ a.sh
@@ -1,20 +1,18 @@
 cd ..
 
 echo "fichiers trouvés :"
-for i in *
+for i in *(.on)
 do
-  [ -f "$i" ] || continue
   echo "<$i>"
 done
 
 echo -n "indice de début : "
 read n_start
-n=$n_start
 
+n=$n_start
 echo "aperçu :"
-for i in *
+for i in *(.on)
 do
-  [ -f "$i" ] || continue
 	ext="${i:e}"
 	if [[ -n "$ext" ]]
 	then
@@ -29,10 +27,15 @@ echo "press enter to continue, ctrl-c to abort"
 read tmp
 
 n=$n_start
-for i in *
+for i in *(.on)
 do
-  [ -f "$i" ] || continue
-	mv -- "$i" "$n.$ext"
+	ext="${i:e}"
+	if [[ -n "$ext" ]]
+	then
+		mv "$i" "$n.$ext"
+	else
+		mv "$i" "$n"
+	fi
 	((n++))
 done
 
